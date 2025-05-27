ed85b10a81a0be4920e8af588e82af97f8a1c537
equinox-1092 <sdiaz1234di@gmail.com>
2025-05-26 22:14:49 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { GifApp } from "./GifApp/pages/GifApp"
+import { SideNavigationBar } from "./public/components/side-navigation-bar"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <SideNavigationBar/>
+      <GifApp/>
+    </>
+  );
+}
+
+export default App
